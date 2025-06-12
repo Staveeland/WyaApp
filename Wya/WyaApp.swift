359b7d2a03684610a1572a0349dd299a2f820a28
Staveeland <staveland@live.no>
2025-06-12 16:30:31 +0200

    Initial Commit

---
+++ Wya/WyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WyaApp.swift
+//  Wya
+//
+//  Created by Petter Staveland on 12/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct WyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
